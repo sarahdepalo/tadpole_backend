98e71c167f372ed83ceafe3ee8fa81df1dd7c01d
Sarah dePalo <sarahdepalo99@gmail.com>
2022-12-08 14:19:18 -0500

    added index route for testing and converted adjusted port and host to work with railway

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+const http = require('http');
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app)
+
+server.listen(port, host, () => {
+    console.log(`Server is running at ${host}:${port}`)
+});
+
+
 //Routes:
 
 //Authentication Routes - Reg & Login:
@@ -14,6 +25,8 @@ app.use("/auth", require("./routes/jwtAuth"));
 //Dashboard Route - for logged in users only:
 app.use("/dashboard", require("./routes/dashboard"));
 
-app.listen(3000, () => {
-    console.log("Server is running at http://localhost:3000")
-});
+// Index route for verification of server functionality
+const rootController = require('./routes/index');
+app.use('/', rootController);
+
+
